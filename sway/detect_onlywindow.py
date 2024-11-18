6b2468dc9606e80c0b5af9064de87c22d63fcd05
Cyril Li <cyrils.li@icloud.com>
2024-11-17 23:48:38 -0600

    fix detect_onlywindow

--- sway/detect_onlywindow.py
+++ sway/detect_onlywindow.py
@@ -1,51 +1,46 @@
-import subprocess, json, time
+import subprocess, json, time, sys
 
 window = {"change": None}
 
 while True:
     pretty = subprocess.run (["swaymsg", "-pt", "get_tree"], capture_output = True)
     pretty.check_returncode ()
-    pretty = [i [4 : ] for i in pretty.stdout.decode ().strip ().split ("\n") if i.startswith ("    ") and " floating_con " not in i]
-    pretty.append ("#")
+    pretty = [i [4 : ] for i in pretty.stdout.decode ().strip ().split ("\n") if i.startswith ("    ")]
 
     raw = subprocess.run (["swaymsg", "-rt", "get_tree"], capture_output = True)
     raw.check_returncode ()
     raw = json.loads (raw.stdout.decode ())
 
-    borders, focused = {}, None
-
+    borders = {}
     def recurse_node (node):
         global borders, focused
+        if node ["name"] in ("__i3", "__i3_scratch"):
+            return
         if not node ["nodes"]:
             if "border" in node:
                 borders [node ["id"]] = node ["border"]
-            if node ["focused"]:
-                focused = node ["id"]
-        for i in node ["nodes"]:
+        for i in node ["nodes"] + node ["floating_nodes"]:
             recurse_node (i)
-    
     recurse_node (raw)
 
-    b_rm, b_add = [], []
-
-    for i, j, k in zip (pretty, pretty [1 : ], pretty [2 : ]):
-        con_id = int (j.split (":") [0].strip () [1 : ])
-        if k.index ("#") <= j.index ("#") and not j.startswith ("#"):
-            if k.index ("#") < j.index ("#") and i.startswith ("#"):
-                if borders.get (con_id) == "normal":
-                    b_rm.append (con_id)
-            elif borders.get (con_id) == "none":
-                b_add.append (con_id)
-
-    for i in b_rm:
-        _ = subprocess.run (["swaymsg", f'[con_id="{i}"]', "focus"])
-        _ = subprocess.run (["swaymsg", "border", "none"])
-    for i in b_add:
-        _ = subprocess.run (["swaymsg", f'[con_id="{i}"]', "focus"])
-        _ = subprocess.run (["swaymsg", "border", "normal"])
-
-    if focused and window ["change"] != "close":
-        focus = subprocess.run (["swaymsg", f'[con_id="{focused}"]', "focus"])
+    containers, onlys = [], []
+    for i in pretty:
+        if i.startswith ("#"):
+            if len (containers) == 1:
+                onlys.append (containers [0])
+            containers = []
+        else:
+            container = i.strip ().split (" ")
+            num = int (container [0] [1 : -1])
+            if num in borders and container [1] == "con":
+                containers.append (num)
+    
+    for k, v in borders.items ():
+        if k in onlys:
+            if v == "normal":
+                _ = subprocess.run (["swaymsg", f'[con_id="{k}"]', "border", "none"])
+        elif v == "none":
+            _ = subprocess.run (["swaymsg", f'[con_id="{k}"]', "border", "normal"])
     
     window ["change"] = None
     while window ["change"] not in ["new", "close", "floating", "title", "move"]:
@@ -53,4 +48,3 @@ while True:
         window.check_returncode ()
         window = json.loads (window.stdout.decode ())
         time.sleep (0.1)
-    time.sleep (0.1)
