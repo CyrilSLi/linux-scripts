0fba339e97912cd8c5e3a1d16ffe2c7125550d8f
Cyril Li <cyrils.li@icloud.com>
2026-04-02 21:33:54 -0500

    update startup

---
+++ sway/startup_containers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+delay="0.1"
+thunar & disown
+sleep $delay
+thunar & disown
+sleep $delay
+foot & disown
+sleep $delay
+ulauncher-toggle
+ulauncher-toggle
+sleep 2
+swaymsg move down
+swaymsg focus up
+swaymsg focus left
+swaymsg move right
+swaymsg move right
+swaymsg rename workspace to 9
+swaymsg move workspace to output eDP-1
