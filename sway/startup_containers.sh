ce1d7444620903bceb7fe8985f5f280e5626593c
Cyril Li <cyrils.li@icloud.com>
2026-05-04 11:15:09 -0500

    only run startup_containers on startup, disable waybar workspace button transition

--- sway/startup_containers.sh
+++ sway/startup_containers.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 delay="0.1"
-thunar & disown
-sleep $delay
-thunar & disown
-sleep $delay
-foot & disown
-sleep $delay
-ulauncher-toggle
-ulauncher-toggle
-sleep 2
-swaymsg move down
-swaymsg focus up
-swaymsg focus left
-swaymsg move right
-swaymsg move right
-swaymsg rename workspace to 9
-swaymsg move workspace to output eDP-1
+if ! pgrep -x "thunar" > /dev/null; then
+    thunar & disown
+    sleep $delay
+    thunar & disown
+    sleep $delay
+    foot & disown
+    sleep $delay
+    ulauncher-toggle
+    ulauncher-toggle
+    sleep 2
+    swaymsg move down
+    swaymsg focus up
+    swaymsg focus left
+    swaymsg move right
+    swaymsg move right
+    swaymsg rename workspace to 9
+    swaymsg move workspace to output eDP-1
+fi
